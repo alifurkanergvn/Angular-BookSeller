491202c3ce1ffacf19e5a0739407104b02542dde
Ali Furkan ERGÜVEN <alifurkanerguven@gmail.com>
2024-07-28 00:03:01 +0300

    Added User Model Class

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,11 @@
+//Export ön ekiyle diğer sınıflardan erişilebilir oluyor.
+import {Role} from "./role.enum";
+
+export class User {
+  id: number |undefined;
+  username: string = "";
+  password: string = "";
+  name: string = "";
+  token: string = "";
+  role: Role = Role.USER;
+}
