26ef35f7ddc7a7bef6ef2d48eafe651f8777a965
Ali Furkan ERGÜVEN <alifurkanerguven@gmail.com>
2024-08-11 14:42:28 +0300

    After login change navbar for user role

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {User} from "./models/user.model";
+import {AuthenticationService} from "./services/authentication.service";
+import {Router} from "@angular/router";
+import {Role} from "./models/role.enum";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-book-seller';
+  //Login işleminden sonra kullanıcı adını göstermek ve kullanıcı rolü bilmek için lazım
+  currentUser: User = new User();
+
+  constructor(private authenticationService:AuthenticationService, private router:Router) {
+    //currentUser ı Observable olarak almalıyız çünkü currentUser'ın değeri değişir değişmez componentta onu yakalamak istiyoruz
+    this.authenticationService.currentUser.subscribe(data=> {
+      this.currentUser = data;
+    })
+  }
+
+  // === hem veri tipi hem değerinin eşitliğini kontrol eder. Role admin ise true döner
+  isAdmin(){
+    return this.currentUser?.role === Role.ADMIN;
+  }
+
+  logOut(){
+    this.authenticationService.logOut();
+    this.router.navigate(['/login']);
+  }
+
 }
