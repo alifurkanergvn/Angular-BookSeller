d16afa96f82e7834f7cd37b23eb3964c0cb779cf
Ali Furkan ERGÜVEN <alifurkanerguven@gmail.com>
2024-08-18 15:20:58 +0300

    Added All Books table in Admin page

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Book} from "../../models/book.model";
+import {BookService} from "../../services/book.service";
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  bookList: Array<Book> = [];
+
+  constructor(private bookService: BookService) { }
 
   ngOnInit(): void {
+    this.bookService.getAllBooks().subscribe(data => {
+      this.bookList = data;
+    });
   }
 
 }
