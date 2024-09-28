03e073a308dc62d9f1a06c4d4151f0297c834c7f
Ali Furkan ERGÜVEN <alifurkanerguven@gmail.com>
2024-09-28 23:55:31 +0300

    Added AdminComponent child-parent relations, and showing new BookComponent Modal for saveBook method.

---
+++ src/app/admin/book/book.component.ts
@@ -0,0 +1,34 @@
+import {Component} from '@angular/core';
+import {Book} from "../../models/book.model";
+import {BookService} from "../../services/book.service";
+
+declare var $: any;  //Jquary tanımı
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent {
+
+  book: Book = new Book();
+  errorMessage: string = "";  //Kitap kaydederken sunucuya gönderdiğimiz isteklerde hata oluşursa hatayı göstereceğiz
+
+  constructor(private bookService: BookService) {
+  }
+
+  saveBook() {
+    this.bookService.saveBook(this.book).subscribe(data => {
+      //...
+    }, error => {
+      this.errorMessage = 'Unexpected error occured when saving book.';
+      console.log(error);
+    })
+  }
+
+  //Diyalogu gösterme ve gizleme fonksiyonu bookComponent html indeki id ye bağladık
+  showBookModal() {
+    $('#bookModal').modal('show')
+  }
+
+}
